ef3fda897be671160cb654c78b8892c0fbd5b693
Callum Fallows <callum_fallows@hotmail.co.uk>
2024-11-23 20:58:35 +0000

    removed unused packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient } from '@tanstack/react-query'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
@@ -12,13 +10,10 @@ function App() {
     return (
         <>
             <BrowserRouter>
-                <QueryClientProvider client={queryClient}>
-                    <ReactQueryDevtools />
-                    <Routes>
-                        <Route path="" element={<HomePage />} />
-                        <Route path="/" element={<HomePage />} />
-                    </Routes>
-                </QueryClientProvider>
+                <Routes>
+                    <Route path="" element={<HomePage />} />
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
             </BrowserRouter>
         </>
     )
